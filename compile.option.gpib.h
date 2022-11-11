05e07cafb4aa220b53b6a6694ee2b9db5bed0389
LAPTOP-FKFPMU57\daniel.lee <yukin@yukin.pl>
2022-11-11 11:56:54 +0900

    add compile option - lib reference

---
+++ compile.option.gpib.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#pragma comment( lib , "C:/Program Files/IVI Foundation/VISA/Win64/ktvisa/Lib_x64/msc/visa32.lib" )
+#pragma include_alias ( "visa.h" , "C:/Program Files/IVI Foundation/VISA/Win64/ktvisa/include/visa.h" )
+#pragma include_alias ( "visaext.h" , "C:/Program Files/IVI Foundation/VISA/Win64/ktvisa/include/visaext.h" )
+#pragma include_alias ( "visatype.h" , "C:/Program Files/IVI Foundation/VISA/Win64/ktvisa/include/visatype.h" )
+#pragma include_alias ( "vpptype.h" , "C:/Program Files/IVI Foundation/VISA/Win64/ktvisa/include/vpptype.h" )
